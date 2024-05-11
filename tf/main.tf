b60533a67176394aa1fde0e3f19ce22fadf13a5d
xNoix <sapdoscofre@gmail.com>
2024-05-11 18:55:32 -0400

    new provider and ec2 config

--- tf/main.tf
+++ tf/main.tf
@@ -1,5 +1,8 @@
-resource "aws_ssm_parameter" "foo" {
-  name  = "foo"
-  type  = "String"
-  value = "barr"
+resource "aws_instance" "mi_instancia" {
+  ami           = "ami-0c94855ba95c574c8" # Esta es la AMI para Ubuntu Server 20.04 LTS en us-east-1
+  instance_type = "t2.micro" # Este tipo de instancia está dentro del Free Tier
+
+  tags = {
+    Name = "MiInstancia"
+  }
 }
--- tf/providers.tf
+++ tf/providers.tf
@@ -1,3 +1,3 @@
-terraform {
-  backend "s3" {}
+provider "aws" {
+  region = "us-east-1"
 }
