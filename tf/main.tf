ea294bca75a5a0b10a0da2c6f7ed6c314db87601
xNoix <sapdoscofre@gmail.com>
2024-05-11 19:30:23 -0400

    new ami version

--- tf/main.tf
+++ tf/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "mi_instancia" {
-  ami           = "ami-0c94855ba95c574c8" # Esta es la AMI para Ubuntu Server 20.04 LTS en us-east-1
-  instance_type = "t2.micro" # Este tipo de instancia está dentro del Free Tier
+  ami           = "ami-04b70fa74e45c3917" # Esta es la AMI para Ubuntu Server 20.04 LTS en us-east-1
+  instance_type = "t2.micro"              # Este tipo de instancia está dentro del Free Tier
 
   tags = {
     Name = "MiInstancia"
